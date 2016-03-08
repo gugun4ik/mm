9f95716afca3f4dd81db9f579b528c6dc5d1f554
gugun4ik <zyujkm@mail.ru>
2016-03-08 20:57:42 +0200

    change in header.php

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
 		<ul>
 			<li class="current_page_item"><a href="#" accesskey="1" title="">Homepage</a></li>
 			<li><a href="#" accesskey="2" title="">Portfolio</a></li>
-			<li><a href="#" accesskey="3" title="">About Us</a></li>
+			<!-- <li><a href="#" accesskey="3" title="">About Us</a></li> -->
 			<li><a href="#" accesskey="4" title="">Careers</a></li>
 			<li><a href="#" accesskey="5" title="">Contact Us</a></li>
 		</ul>
